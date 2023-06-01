fa6358e0dd57688d3d6829e8399b9fcee40a0bf0
codeyourwaytofreedom <can.kolej@gmail.com>
2023-06-01 17:44:49 +0300

    webcam stream provided to the model
    works well
    
    Keep going
    
    Remember why you started

--- components/Homie.tsx
+++ components/Homie.tsx
@@ -8,6 +8,7 @@ import bird from "./bird.jpeg";
 import h from "./h.jpeg";
 import b from "./b.jpeg";
 import boat from "./boat.jpeg";
+import c from "./c.webp";
 
 import Image from "next/image";
 import { load } from "@tensorflow-models/coco-ssd";
@@ -15,17 +16,19 @@ import { load } from "@tensorflow-models/coco-ssd";
 const Homie = () => {
   const im = useRef<HTMLImageElement>(null);
   const canv = useRef<HTMLCanvasElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   const [result, setResult] = useState<DetectedObject[]>([]);
 
   useEffect(() => {
     (async () => {
       // Load the model.
-      const model = await load();
+      //const model = await load();
 
       // Classify the image.
-      const predictions = await model.detect(im.current!);
-      setResult(predictions);
-      console.log(predictions)
+      //const predictions = await model.detect(im.current!);
+
+      //console.log(predictions)
 
       // Draw rectangles on the canvas
       const canvas = canv.current!;
@@ -34,7 +37,7 @@ const Homie = () => {
       ctx!.clearRect(0, 0, canvas.width, canvas.height);
 
       // Draw rectangles for each prediction
-      predictions.forEach((prediction) => {
+/*       predictions.forEach((prediction) => {
         const { bbox } = prediction;
         const [x, y, width, height] = bbox;
 
@@ -55,15 +58,35 @@ const Homie = () => {
         ctx!.beginPath();
         ctx!.rect(rectX, rectY, rectWidth, rectHeight);
         ctx!.stroke();
-      });
+      }); */
+
     })();
   }, []);
+
+/*   useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Access the webcam stream
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          // Set the video source to the stream
+          videoRef.current!.srcObject = stream;
+        }).then( async ()=> {
+          const model = await load();
+          const predictions = await model.detect(videoRef.current!);
+          console.log(predictions)
+        })
+        .catch((error) => {
+          console.log("Error accessing webcam:", error);
+        });
+    }
+  }, []); */
 
   return (
     <>
       <div className={m.frame}>
         <div className={m.frame_kernel}>
-          <Image src={boat} alt="serve" ref={im} />
+          <video ref={videoRef} autoPlay playsInline />
           <canvas ref={canv} />
         </div>
       </div>

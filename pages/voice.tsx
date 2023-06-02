ecdcc96883e377833b01059a213f75beace0287c
codeyourwaytofreedom <can.kolej@gmail.com>
2023-06-02 18:04:50 +0300

    Two models tested in two routes.
    Both works properly.
    
    Great step forward.
    
    Next step: combine two models in one project.
    
    Keep going.
    
    Remember why you started.

--- pages/voice.tsx
+++ pages/voice.tsx
@@ -4,10 +4,11 @@ import '@tensorflow/tfjs-backend-webgl';
 import { useState } from "react";
 import * as speech from "@tensorflow-models/speech-commands";
 import { SpeechCommandRecognizer } from "@tensorflow-models/speech-commands";
+import * as tf from "@tensorflow/tfjs";
 
 const Voice = () => {
   const [model, setModel] = useState<SpeechCommandRecognizer>();
-  const [action, setAction] = useState(null);
+  const [action, setAction] = useState<string>();
   const [labels, setLabels] = useState<string[]>();
 
   const loadModel = async () =>{
@@ -25,14 +26,39 @@ const Voice = () => {
 
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     loadModel();
   }, []);
+  
+  function argMax(arr) {
+    return arr.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+  }
 
+  const handleSpeakButton = async () => {
+    if (model) {
+      // Start listening for speech commands
+      model.listen(
+        (result) => {
+          // Process the recognized speech command result
+          console.log(result);
+          setAction(labels![argMax(Object.values(result.scores))]);
+          return Promise.resolve();
+        },
+        {
+          includeSpectrogram: true,
+          probabilityThreshold: 0.75,
+        }
+      );
+      setTimeout(() => model.stopListening(), 10e3);
+    }
+  };
+  
 
   return (
     <>
+      {action ? <div>{action}</div> : <div>No Command Detected</div>}
       <h1>Voice commands to be tested here</h1>
+      <button onClick={handleSpeakButton}>Press to Speak</button>
     </>
   );
 };

d3453549be96876655d1618d2c431288aa0ca2da
codeyourwaytofreedom <can.kolej@gmail.com>
2023-06-09 21:52:26 +0300

    Project logic in place.
    Image slider via sound commands.
    
    No input devices!
    
    Looks great.
    
    Functionality to be added.
    
    Keep going.
    
    Remember why you started.

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,14 +1,21 @@
+import Image from "next/image";
+import { useState } from "react";
 import g from "../styles/Main.module.css";
 
 
 const Game = () => {
+    const [index,setIndex] = useState<number>(4)
     return (
         <>
             <div className={g.game}>
                 <div className={g.game_mainline}>
                     <div className={g.game_mainline_jumper}>
-                        
+                        <Image alt={"no"} src={`/b${index}.png`} width={100} height={100}/>
                     </div>
+                </div>
+                <div className={g.game_arrows}>
+                    <div><button>&#10094;</button></div>
+                    <div><button>&#10095;</button></div>
                 </div>
             </div>
         </>
